e82f9fd0707d840d467ed55eac9852b7eaaf64ee
johnfkneafsey <johnfkneafsey@gmail.com>
2017-01-09 14:11:05 -0500

    First Exercise Done

---
+++ js/components/card.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+export default function Card () {
+    return (
+        <div className="card">This is a card!</div>
+    );
+}
---
+++ js/components/list.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Card from './card';
+
+export default function List () {
+    return (
+        <div className="list">
+            <Card />
+            <Card />
+            <Card />        
+        </div>
+    )
+}
+
